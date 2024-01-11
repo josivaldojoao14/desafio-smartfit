e340e2c981dab5bd89cea989e4954c67437d80e8
Josivaldo Joao <josivaldojoao14@gmail.com>
2024-01-10 23:19:31 -0300

    interfaces added

---
+++ src/app/types/locations.interface.ts
@@ -0,0 +1,15 @@
+import { Schedule } from "./schedule.interface";
+
+export interface Locations {
+  id: number,
+  title: string,
+  content: string,
+  opened: boolean,
+  mask: string,
+  towel: string,
+  fountain: string,
+  locker_room: string,
+
+  schedules: Schedule[]
+}
+
